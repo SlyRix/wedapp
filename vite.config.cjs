1c48c37728fe5e3b74b7bdda432e598025c38ce8
Rushel <rushelsilvester@outlook.com>
2024-10-29 19:17:18 +0100

    vite

--- vite.config.cjs
+++ vite.config.cjs
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Add this to allow network access
+    port: 5173,  // Default port
+    strictPort: true, // Don't try other ports if 5173 is taken
+  }
 })
